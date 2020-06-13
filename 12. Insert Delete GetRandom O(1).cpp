33f64fd4b38e1363e58e62cc8e19d9c714affd45
balkisha88 <64643812+balkisha88@users.noreply.github.com>
2020-06-13 11:54:43 +0530

    Create 12. Insert Delete GetRandom O(1).cpp
    Design a data structure that supports all following operations in average O(1) time.
    
    insert(val): Inserts an item val to the set if not already present.
    remove(val): Removes an item val from the set if present.
    getRandom: Returns a random element from current set of elements. Each element must have the same probability of being returned.
    Example:
    
    // Init an empty set.
    RandomizedSet randomSet = new RandomizedSet();
    
    // Inserts 1 to the set. Returns true as 1 was inserted successfully.
    randomSet.insert(1);
    
    // Returns false as 2 does not exist in the set.
    randomSet.remove(2);
    
    // Inserts 2 to the set, returns true. Set now contains [1,2].
    randomSet.insert(2);
    
    // getRandom should return either 1 or 2 randomly.
    randomSet.getRandom();
    
    // Removes 1 from the set, returns true. Set now contains [2].
    randomSet.remove(1);
    
    // 2 was already in the set, so return false.
    randomSet.insert(2);
    
    // Since 2 is the only number in the set, getRandom always return 2.
    randomSet.getRandom();

---
+++ 12. Insert Delete GetRandom O(1).cpp
@@ -0,0 +1,54 @@
+class RandomizedSet {
+public:
+    /** Initialize your data structure here. */
+    vector<int> v;
+    unordered_map<int,int> mp;
+    RandomizedSet() {
+        
+    }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    bool insert(int val) {
+        
+        if(mp.find(val)==mp.end())
+        {
+            v.push_back(val);
+            mp[val]=v.size()-1; 
+            return true;
+        }
+        return false;
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    bool remove(int val) {
+         if(mp.find(val)!=mp.end())
+        {
+             int index=mp[val];
+             // int temp=v[index];
+             // v[index]=v[v.size()-1];
+             // v[v.size()-1]=temp;
+             v[index]=v[v.size()-1];
+             v.pop_back();
+             mp[v[index]]=index;
+             mp.erase(val);
+             return true;
+         }
+        return false;
+        
+    }
+    
+    /** Get a random element from the set. */
+    int getRandom() {
+        
+        int index=rand()%(v.size());
+        return v[index];
+    }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
